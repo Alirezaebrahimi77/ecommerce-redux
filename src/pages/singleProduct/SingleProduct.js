cb4acb6a99a12a89191f7756361af9ae3c935e68
Alireza <alireza27163@gmail.com>
2021-12-30 11:52:53 +0200

    developed product page

--- src/pages/singleProduct/SingleProduct.js
+++ src/pages/singleProduct/SingleProduct.js
@@ -1,48 +1,50 @@
-import React, {useEffect} from 'react'
-import "./singleProduct.css"
-import { useDispatch, useSelector } from 'react-redux'
-import {getSingleProduct} from "../../actions/singleProductActions"
-import { useParams } from 'react-router-dom';
-import Loader from '../../components/Loader/Loader';
+import React, { useEffect } from "react";
+import "./singleProduct.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getSingleProduct } from "../../actions/singleProductActions";
+import { useParams } from "react-router-dom";
+import Loader from "../../components/Loader/Loader";
+import StarRatings from "react-star-ratings";
 
 const SingleProduct = () => {
-    const dispatch = useDispatch();
-    const {loading, error, singleProduct} = useSelector(state => state.singleProduct)
-    const {id} = useParams();
-    useEffect(()=> {
-        dispatch(getSingleProduct(id))
-    },[dispatch, error])
+  const dispatch = useDispatch();
+  const { loading, error, singleProduct } = useSelector(
+    (state) => state.singleProduct
+  );
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(getSingleProduct(id));
+  }, [dispatch, error]);
 
-
-    return (
-        
-        <div className='singleProductsWrapper'>
-            {loading ? <Loader /> : 
-            <>
-            <div className="singleProductLeft">
-                <img src={singleProduct.image} alt={singleProduct.title} />
-                
-
-
-
+  return (
+    <div className="singleProductsWrapper">
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="singleProductLeft">
+            <img src={singleProduct.image} alt={singleProduct.title} />
+          </div>
+          <div className="singleProductRight">
+            <div className="singleProductContent">
+              <p className="singleProductTitle">{singleProduct.title}</p>
+              <StarRatings
+                rating={singleProduct.rating.rate}
+                starDimension="18px"
+                starSpacing="2px"
+                starRatedColor= "gold"
+              />
+              <span style={{color: "grey"}}> ({singleProduct.rating.count})</span>
+              <p className="singleProductPrice">{`${singleProduct.price}$`}</p>
+              <p className="singleProductDesc">{singleProduct.description}</p>
             </div>
 
-            <div className="singleProductRight">
+            <button className="addToCartBtn">Add to Cart</button>
+          </div>{" "}
+        </>
+      )}
+    </div>
+  );
+};
 
-                <div className="singleProductContent">
-                    <p>{singleProduct.title}</p>
-                    <p>{singleProduct.price}</p>
-                    <p>{singleProduct.description}</p>
-                </div>
-                
-            </div>            </>}
-            
-            
-
-            
-            
-        </div>
-    )
-}
-
-export default SingleProduct
+export default SingleProduct;
